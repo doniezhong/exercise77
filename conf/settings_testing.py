c1869f8505e2831208b85499a79b5806cabb516b
Donie <donie@canway.net>
2019-07-08 16:58:34 +0800

    u

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -13,9 +13,5 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # 默认用mysql
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
-        'HOST': '192.168.163.212',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
     },
 }
