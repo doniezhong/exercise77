80d4976596536e9e61ddaf8a234c8de656c5f27e
Donie <donie@canway.net>
2019-07-08 14:34:48 +0800

    u

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -2,16 +2,17 @@
 """
 用于测试环境的全局配置
 """
-from settings import APP_ID
+import os
 
+from settings import APP_ID, BASE_DIR
 
 # ===============================================================================
 # 数据库设置, 测试环境数据库设置
 # ===============================================================================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'ENGINE': 'django.db.backends.sqlite3',  # 默认用mysql
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
         'HOST': '192.168.163.212',                   		   # 数据库HOST
